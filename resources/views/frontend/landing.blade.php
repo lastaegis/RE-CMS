7ed0ac066ccd45652fae63a8b743f08aa561648c
Ibnu Aulia Nugraha A <ibnu@leapup.id>
2017-10-03 12:00:54 +0700

    New change interface

--- resources/views/frontend/landing.blade.php
+++ resources/views/frontend/landing.blade.php
@@ -4,14 +4,14 @@
 @endsection
 @section("content")
 <div class="ui container">
-    <div class="ui stackable grid">
+    <div class="ui stackable centered grid">
         @foreach($cafeList as $cafe)
-            <div class="four wide column">
+            <div class="sixteen wide column">
                 <div class="ui special cards">
-                    <div class="card">
+                    <div class="card fluid">
                         <div class="content">
                             <div class="right floated meta">14h</div>
-                            <img class="ui avatar image" src="https://semantic-ui.com/images/avatar/large/elliot.jpg"> Elliot
+                            <b>{{ $cafe->name }}</b>
                         </div>
                         <div class="blurring dimmable image">
                             <div class="ui dimmer">
@@ -26,10 +26,10 @@
                         <div class="content">
                             <span class="right floated">
                               <i class="heart outline like icon"></i>
-                              17 likes
+                              Open Hour
                             </span>
                             <i class="comment icon"></i>
-                            3 comments
+                            Price Range
                         </div>
                     </div>
                 </div>
@@ -37,23 +37,18 @@
         @endforeach
     </div>
 </div>
-<div class="ui modal" id="modal-post-descriptions">
+<div class="ui fullscreen modal" id="modal-post-descriptions">
     <i class="close icon"></i>
-    <div class="header">
-        FOOD
-    </div>
-    <div class="image scrolling content">
-        <div class="ui medium image">
+    <div class="image scrolling content no-padding">
+        <div class="ui large image">
             <img id="image" src="https://semantic-ui.com/images/wireframe/image.png">
         </div>
         <div class="description">
             <div class="ui header">Description</div>
             <div id="description">This is an example of expanded content that will cause the modal's dimmer to scroll</div>
+            <div class="ui divider"></div>
+            <a href="#" class="ui blue button" id="full-post">Read More</a>
         </div>
-    </div>
-    <div class="actions">
-        <div class="ui red button close-button">Close</div>
-        <a href="#" class="ui blue button" id="full-post">Read More</a>
     </div>
 </div>
 @endsection
--- resources/views/frontend/post.blade.php
+++ resources/views/frontend/post.blade.php
@@ -3,46 +3,40 @@
     @include("frontend.core.menu")
 @endsection()
 @section("content")
-<div class="ui container">
-    <div class="ui stackable grid">
-        <div class="row">
-            <div class="six wide column">
-                <img class="ui fluid image" src="https://semantic-ui.com/images/wireframe/image.png">
+    <div class="ui container post">
+        <div class="ui stackable grid">
+            <div class="sixteen wide column grid center aligned">
+                <h1>{{ $cafeData->name }}</h1>
             </div>
-            <div class="ten wide column">
-                <div class="ui piled segment">
-                    <h4 class="ui header">{{ $cafeData->name }}</h4>
-                    <p>
-                        <i class="location arrow icon"></i>
-                        <a href="{{ $cafeData->maps }}"> {{ $cafeData->address }}</a>
-                    </p>
-                    <p>
-                        <i class="hourglass full icon"></i> {{ $cafeData->open_hour }} - {{ $cafeData->close_hour }}
-                    </p>
-                    <p>
-                        <i class="money icon"></i> Rp. {{ number_format($cafeData->start_price,0,".",".") }} - Rp. {{ number_format($cafeData->end_price,0,".",".") }}
-                    </p>
-                    <p><b>Descriptions: </b></p>
-                    {!! $cafeData->notes !!}
-                </div>
-            </div>
-        </div>
-        <div class="row">
             <div class="sixteen wide column">
-                <div class="ui segment">
-                    <iframe src="{{ $cafeData->maps }}" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <div class="row">
+                    <img class="ui fluid image" src="{{ URL::asset("storage/app/".$cafeData->thumbnail[0]->detail->path) }}">
+                </div>
+                <div class="row story">
+                    {!! $cafeData->notes !!}
+                    <p><i class="map icon"></i> <a href="{{ $cafeData->maps }}"> {{ $cafeData->address }}</a></p>
+                    <p><i class="money icon"></i> Rp. {{ number_format($cafeData->start_price,0,".",".") }} - Rp. {{ number_format($cafeData->end_price,0,".",".") }}</p>
+                    <p><i class="hourglass full icon"></i> {{ date('h:i', strtotime($cafeData->open_hour)) }} - {{ date('h:i', strtotime($cafeData->close_hour)) }}</p>
                 </div>
             </div>
-        </div>
-        <div class="row" id="light-gallery">
-            @foreach($cafeData->gallery as $gallery)
-                <div class="four wide column" style="padding-bottom: 5%;">
-                    <div class="pictures" data-src="{{ URL::asset('storage/app/'.$gallery->detail->path) }}">
-                        <img class="ui fluid image" src="{{ URL::asset('storage/app/'.$gallery->detail->path) }}">
+            <div class="sixteen wide column grid center aligned">
+                <h4>{{ $cafeData->name }} Image</h4>
+            </div>
+            <div class="row" id="light-gallery">
+                @foreach($cafeData->gallery as $gallery)
+                    <div class="four wide column" style="padding-bottom: 5%;">
+                        <div class="pictures" data-src="{{ URL::asset('storage/app/'.$gallery->detail->path) }}">
+                            <img class="ui fluid image" src="{{ URL::asset('storage/app/'.$gallery->detail->path) }}">
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
+            <div class="sixteen wide column grid center aligned">
+                <h4>{{ $cafeData->name }} Maps</h4>
+            </div>
+            <div class="row">
+                <iframe src="{{ $cafeData->maps }}" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
         </div>
     </div>
-</div>
 @endsection
